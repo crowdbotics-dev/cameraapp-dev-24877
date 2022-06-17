2e6a1cbd1de1fcaa4c123736b49ecefe2541712a
Crowdbotics <engineering@crowdbotics.com>
2022-06-17 17:09:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cameraapp_dev_24877/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cameraapp_dev_24877;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cameraapp_dev_24877";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cameraapp_dev_24877",
+  url: "https://cameraapp_dev_24877.botics.co",
+  api: "https://cameraapp_dev_24877.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
